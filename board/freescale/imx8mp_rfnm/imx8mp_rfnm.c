8670422935cde86320d5004d349a9f3635dd31c3
Davide Cavion <davide@rfnm.io>
2023-06-17 10:16:30 +0200

    Enable ethernet at boot time

--- board/freescale/imx8mp_rfnm/imx8mp_rfnm.c
+++ board/freescale/imx8mp_rfnm/imx8mp_rfnm.c
@@ -452,30 +452,35 @@ int board_phy_config(struct phy_device *phydev)
 #define DISPMIX				13
 #define MIPI				15
 
-static iomux_v3_cfg_t ss_mux_rfnm_pwr[] = {
+static iomux_v3_cfg_t ss_mux_rfnm[] = {
 	MX8MP_PAD_GPIO1_IO12__GPIO1_IO12 | MUX_PAD_CTRL(NO_PAD_CTRL),
 	MX8MP_PAD_NAND_READY_B__GPIO3_IO16 | MUX_PAD_CTRL(NO_PAD_CTRL),
+	MX8MP_PAD_SPDIF_EXT_CLK__GPIO5_IO05 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
 #define PWR_EN_33V IMX_GPIO_NR(1, 12)
 #define Si5510_PWR_EN IMX_GPIO_NR(3, 16)
-
+#define ENET_nRST IMX_GPIO_NR(5, 5)
 
 int board_init(void)
 {
 	struct arm_smccc_res res;
 
+	imx_iomux_v3_setup_multiple_pads(ss_mux_rfnm, ARRAY_SIZE(ss_mux_rfnm));
 
-	imx_iomux_v3_setup_multiple_pads(ss_mux_rfnm_pwr, ARRAY_SIZE(ss_mux_rfnm_pwr));
-
-	gpio_request(PWR_EN_33V, "pwr_en_33v");
+	gpio_request(PWR_EN_33V, "PWR_EN_33V");
 	gpio_direction_output(PWR_EN_33V, 1);
 
-	gpio_request(Si5510_PWR_EN, "si5510_pwr_en");
+	gpio_request(Si5510_PWR_EN, "Si5510_PWR_EN");
 	gpio_direction_output(Si5510_PWR_EN, 1);
 
 
 	printf("Done pwr en init\n");
+
+	gpio_request(ENET_nRST, "ENET_nRST");
+	gpio_direction_output(ENET_nRST, 1);
+
+
 
 
 
